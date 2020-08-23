67701a6b3c0184fabb056e25f202ffcbe1e18a24
Daniel Khaapamyaki <dasky@hey.com>
2020-08-24 01:50:18 +0200

    add modal for history images

--- bird_app_ui/lib/bird_app_ui_web/live/snap_history_live.ex
+++ bird_app_ui/lib/bird_app_ui_web/live/snap_history_live.ex
@@ -3,7 +3,7 @@ defmodule BirdAppUiWeb.SnapHistoryLive do
   alias BirdAppUi.DB
 
   @impl true
-  def mount(_params, _session, socket) do
+  def mount(params, _session, socket) do
     measurements = BirdAppHardware.Dht.read(Dht4)
     snaps = DB.entries_count()
     messages = DB.entries()
@@ -27,5 +27,46 @@ defmodule BirdAppUiWeb.SnapHistoryLive do
     )
 
     {:noreply, socket}
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  def apply_action(socket, :index, _params) do
+    assign(socket, show_modal: false)
+  end
+
+  def apply_action(socket, :modal, params) do
+    assign(socket, show_modal: true, key: Map.get(params, "key"))
+  end
+
+  def apply_action(socket, _live_action, _params) do
+    push_patch(socket,
+      to: Routes.snap_history_path(socket, :index),
+      replace: true
+    )
+  end
+
+  def handle_event("open-modal", %{"key" => key}, socket) do
+    {:noreply,
+     socket
+     |> assign(:key, key)
+     |> push_patch(
+       to: Routes.snap_history_path(socket, :modal, key),
+       replace: true
+     )}
+  end
+
+  def handle_info(
+        {BirdAppUiWeb.ModalComponent, :button_clicked, %{action: "close"}},
+        socket
+      ) do
+    {:noreply,
+     push_patch(socket,
+       to: Routes.snap_history_path(socket, :index),
+       replace: true
+     )}
   end
 end
