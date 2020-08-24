6b60e0fdcd5c9683e74bd5c03942aa16c14c87fa
Daniel Khaapamyaki <dasky@hey.com>
2020-08-24 13:52:12 +0200

    add modal

--- bird_app_ui/lib/bird_app_ui_web/live/components/modal_component.ex
+++ bird_app_ui/lib/bird_app_ui_web/live/components/modal_component.ex
@@ -19,37 +19,18 @@ defmodule BirdAppUiWeb.ModalComponent do
     ~L"""
     <div class="fixed z-10 inset-0 overflow-y-auto">
       <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-        <!--
-          Background overlay, show/hide based on modal state.
-
-          Entering: "ease-out duration-300"
-            From: "opacity-0"
-            To: "opacity-100"
-          Leaving: "ease-in duration-200"
-            From: "opacity-100"
-            To: "opacity-0"
-        -->
         <div class="fixed inset-0 transition-opacity">
           <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
         </div>
 
         <!-- This element is to trick the browser into centering the modal contents. -->
         <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>&#8203;
-        <!--
-          Modal panel, show/hide based on modal state.
 
-          Entering: "ease-out duration-300"
-            From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            To: "opacity-100 translate-y-0 sm:scale-100"
-          Leaving: "ease-in duration-200"
-            From: "opacity-100 translate-y-0 sm:scale-100"
-            To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-        -->
         <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
           <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
             <div class="sm:flex sm:items-start">
               <div class="mt-3 sm:mt-0">
-                <img class="rounded" src="<%= Routes.snap_path(BirdAppUiWeb.Endpoint, :image, @key) %>" alt="Camera Snapshot" />
+                <img class="rounded" src="<%= @image %>" alt="Camera Snapshot" />
               </div>
             </div>
           </div>
